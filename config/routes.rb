c9b4c7da5cd306d895fcfc3dba67692d595812a3
yukitaka7478 <yukitaka897@gmail.com>
2022-06-03 22:53:18 +0900

    evett詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "evetts#index"
-  resources :evetts, only: [:index, :new, :create]
-  get "evetts/index_freind"
-  get "evetts/index_user"
+  resources :evetts, only: [:index, :new, :create, :show]
+  get "evetts/index"
 end
