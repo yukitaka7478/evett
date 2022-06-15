de5a6dae415d607c90d2a844da33f2f4f3f69f49
yukitaka7478 <yukitaka897@gmail.com>
2022-06-15 15:44:52 +0900

    フォロー一覧表示機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       @card = customer.cards.first
       
       @following_users = @user.following_user
-      @followed_users = @user.followed_user
+      @followed_users = @user.followed_user.order('created_at DESC')
     end
 
     @evetts = Evett.where(user_id: @user.id).order('created_at DESC')
